b7233fa76725413e482da5af6ee6ca65ac97d7f7
linushjeltman <linus.hjeltman@hotmail.com>
2022-01-14 12:02:25 +0100

    Removed unused code

--- wat.js
+++ wat.js
@@ -12,19 +12,16 @@ const initial = {
 
   frustum: {
     fov: 75,
-    aspect: 2,  // the canvas default
+    aspect: 2, 
     near: 0.1,
     far: 50,
   },
 
   attributes: {
     speed: 1,
-    amplitude: 1,
-
+    amplitude: 0.1,
   }
-
 }
-
 
 function main() {
   const canvas = document.querySelector('#c');
@@ -45,21 +42,15 @@ function main() {
   scene.background = new THREE.Color( 0x59595959 );
 
   const geometry = new THREE.PlaneGeometry(initial.plane.height, initial.plane.width, initial.plane.heightSegments, initial.plane.widthSegments);
-  const wireframe = new THREE.WireframeGeometry(geometry);
-  const line = new THREE.LineSegments(wireframe);
 
-  //Material with variables controlled by GUI
+  // Material based on phong-shading
   let planematerial = new THREE.MeshPhongMaterial({
     color: 0x4287f5,
     shininess: 50,
-    opacity: 1,
     flatShading: true,
-    //side: THREE.DoubleSide,
+    transparent: true,
+    opacity: 0.8,
   });
-
-  // let planematerial = new THREE.MeshPhongMaterial({
-  //   color: 0xffffff, flatShading: true, vertexColors: true, shininess: 0
-  // });
 
   const planemesh = new THREE.Mesh(geometry, planematerial);
 
@@ -121,10 +112,8 @@ function main() {
   const gui = new GUI();
   gui.add(planemesh.scale, 'x', 0, 10, 0.1).name('width');
   gui.add(planemesh.scale, 'y', 0, 10, 0.1).name('height');
-  gui.add(initial.plane, 'widthSegments', 0, 10, 0.1).name('widthS');
-  gui.add(initial.plane, 'heightSegments', 0, 10, 0.1).name('heigthS');
   gui.add(initial.attributes, 'speed', 0.1, 10, 0.01).name('speed');
-  gui.add(initial.attributes, 'amplitude', 0.1, 2, 0.01).name('amplitude');
+  gui.add(initial.attributes, 'amplitude', 0.001, 1, 0.01).name('amplitude');
 
   //gui.add(line.wSegments ,'widthSegment', 1, 20, 1).name('hs');
 
@@ -133,8 +122,6 @@ function main() {
   ///////////////////////////
   /////////Rendering/////////
   ///////////////////////////
-
-
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -153,14 +140,26 @@ function main() {
 
     frame += 0.01;
     let { array, originalpos, randomValues } = planemesh.geometry.attributes.position
-
     // Moving vertices in Z-direction
     for (let index = 0; index < planemesh.geometry.attributes.position.count; index++) {
       planemesh.geometry.attributes.position.setZ(index, (initial.attributes.amplitude * randomValues[index] * Math.sin(frame * initial.attributes.speed) / 5))
-
     }
-    controls.update();
 
+    // let i = 0, j = 0;
+
+		// 		for ( let ix = 0; ix < 100; ix ++ ) {
+
+		// 			for ( let iy = 0; iy < 100; iy ++ ) {
+
+		// 				array[ i + 1 ] = ( Math.sin( ( ix + frame /100) * 0.03 )  ) +
+		// 								( Math.sin( ( iy + frame /100) * 0.05 )  );
+		// 				i += 3;
+						
+
+		// 			}
+
+		// 		}
+    controls.update();
     planemesh.geometry.attributes.position.needsUpdate = true
 
     if (resizeRendererToDisplaySize(renderer)) {
