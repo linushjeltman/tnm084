7608d0063115f499fc4b43fad9a316cd756d5033
linushjeltman <linus.hjeltman@hotmail.com>
2022-01-15 11:03:58 +0100

    Done ALOT, restructured, made functions for noise, added GUI elements

--- wat.js
+++ wat.js
@@ -8,8 +8,8 @@ import { OrbitControls } from 'https://cdn.skypack.dev/pin/three@v0.134.0-mlfrkS
 
 const initial = {
   plane: {
-    width: 2,
-    height: 2,
+    width: 5,
+    height: 5,
     widthSegments: 100,
     heightSegments: 100,
   },
@@ -22,9 +22,22 @@ const initial = {
   },
 
   attributes: {
-    speed: 1,
+    speed: 2,
     speed2: 1,
-    amplitude: 0.1,
+    noisescale: 1,
+    amplitude: 1,
+    sineFreq: 100,
+    hashenabled: true,
+    modsineenabled: false,
+  },
+  noise: {
+    hash: function hash(i) {
+      let temp = (34*i^2 +10*i)%389;
+      return temp
+    },
+    modSine: function modSine(i,j,freq) {
+      return Math.abs(Math.sin( (i + j) * freq/57,29578779  ))-Math.trunc(Math.abs(Math.sin((i + j)*freq/57,29578779))); // modified sine as in lectures, pi/180 = 1/57,29578779
+    } 
   }
 }
 
@@ -72,20 +85,12 @@ function main() {
 
   scene.add(planemesh);
 
-  const { array } = planemesh.geometry.attributes.position
-  const randomValues = []
-  for (let i = 0; i < array.length; i++) {
-    randomValues.push(Math.random())
-  }
-  planemesh.geometry.attributes.position.randomValues = randomValues
-  planemesh.geometry.attributes.position.originalpos = planemesh.geometry.attributes.position.array
-
   ////////////////////////
   /////////LIGHTS/////////
   ////////////////////////
 
   const light = new THREE.SpotLight(0xffffff, 1.5);
-  light.position.set(0, 500, 2000);
+  light.position.set(500, 500, 2000);
   scene.add(light);
 
   //////////////////////////////////
@@ -128,7 +133,11 @@ function main() {
   gui.add(planemesh.scale, 'y', 0, 10, 0.1).name('height');
   gui.add(initial.attributes, 'speed', 0.1, 10, 0.01).name('speed');
   gui.add(initial.attributes, 'speed2', 0.1, 10, 0.01).name('speed2');
-  gui.add(initial.attributes, 'amplitude', 0.001, 1, 0.01).name('amplitude');
+  gui.add(initial.attributes, 'noisescale', 0.001, 1, 0.01).name('noisescale');
+  gui.add(initial.attributes, 'amplitude', 0, 3, 0.01).name('amplitude');
+  gui.add(initial.attributes, 'sineFreq', 1, 360, 1).name('sineFreq');
+  gui.add(initial.attributes, 'hashenabled').name('hash');
+  gui.add(initial.attributes, 'modsineenabled').name('modsine');
 
   console.log(planemesh);
 
@@ -147,23 +156,24 @@ function main() {
     return needResize;
   }
 
-  let frame = 0;
+
+
+  let frame = 0, mods = 0, rand;
  
   function render() {
-    let { array, originalpos, randomValues, count } = planemesh.geometry.attributes.position
-    let { amplitude, speed, speed2 } = initial.attributes;
-    frame += 0.01;
-    // Moving vertices in Z-direction
-    //Math.sin(frame*Math.PI/180)/10+
-    for (let index = 0; index < count; index++) {
-      planemesh.geometry.attributes.position.setZ(index, (amplitude * randomValues[index] * Math.sin(frame * speed) / 5));
-    }
+    let { array, count } = planemesh.geometry.attributes.position // Access to verticies 
+    let { amplitude,noisescale, speed, speed2, sineFreq, hashenabled, modsineenabled } = initial.attributes; // Acess to values that are being modified by GUI
+    let { hash, modSine } = initial.noise; // Accessing noise-functions
+    
+    frame += 0.01; // This is used to make things move
 
-    let i = 0, j = 0;
+    let i = 0;
     for ( let ix = 0; ix < Math.sqrt(count); ix ++ ) {
       for ( let iy = 0; iy < Math.sqrt(count); iy ++ ) {
-        array[i + 2] += ( Math.sin( ( ix + frame * speed2 ) * 0.6 )/100 ) + 
-        ( Math.sin( ( iy + frame * speed2 ) * 0.4 )/100 );
+        mods = (modsineenabled === true ? modSine(frame, iy, sineFreq)*noisescale/100 : 1);
+        rand =  (hashenabled === true ?  hash(hash(iy)+ix)*noisescale/100 : 1);
+        
+        array[i + 2] = amplitude / 50 *(Math.sin( frame * speed * mods * rand) + Math.sin( ( frame * speed2 + mods + rand) ) ) ;
         i += 3;
       }
     }
